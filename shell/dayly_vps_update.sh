dd3daf2741318bd13bd42931b39b973fde327d64
qcloud <ubuntu@localhost.localdomain>
2024-01-10 14:06:09 +0800

    update shell

--- shell/dayly_vps_update.sh
+++ shell/dayly_vps_update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source $HOME/.bashrc
 
 cd ~/workspace/czsc/
 source venv/bin/activate
 
 # 移除缓存
-# python -m hjw_examples.remove_cache
+python -m hjw_examples.remove_cache
 # 以最新数据分析当天走势
 python -m hjw_examples.day_trend_bc_reverse
