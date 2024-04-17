f0e8a2a3dac10b76bfe6389b06ea404e1884f0a8
hjw <tonnyhjw@126.com>
2024-04-17 15:20:02 +0800

    remove zs1.zd > zs2.zg for 3rd buy

--- hjw_examples/sig.py
+++ hjw_examples/sig.py
@@ -98,7 +98,7 @@ def trend_reverse_ubi(c: CZSC, **kwargs) -> OrderedDict:
                 ubi['direction'] == Direction.Up
                 and len(ubi['fxs']) < 2
                 and ubi['low'] > zs3.zg
-                and zs1.zd > zs2.zg
+                # and zs1.zd > zs2.zg
                 and zs2.zd > zs3.zg
         ):
             estimated_profit = (ubi['high'] - cur_price) / cur_price
