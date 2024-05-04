b3f53c919083230ac896888bbcb2f5599a689a47
qcloud <ubuntu@localhost.localdomain>
2024-05-04 10:11:32 +0800

    周线分析改用背驰

--- shell/week_bot_fx.sh
+++ shell/week_bot_fx.sh
@@ -3,4 +3,5 @@ cd ~/workspace/czsc/
 source venv/bin/activate
 
 # 检测本周强势底分型个股
-python -m hjw_examples.week_bot_fx
+# python -m hjw_examples.week_bot_fx
+python -m hjw_examples.week_trend_bc_reverse
