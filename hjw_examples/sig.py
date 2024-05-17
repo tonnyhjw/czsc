2bdebc0cee51f313031d467ffa5e8d0c47fa0f9f
hjw <tonnyhjw@126.com>
2024-05-17 10:29:29 +0800

    移除日志

--- hjw_examples/sig.py
+++ hjw_examples/sig.py
@@ -294,8 +294,7 @@ def trend_reverse_ubi_dev(c: CZSC, fx_dt_limit: int = 5, **kwargs) -> OrderedDic
         # 提取一买后的bi_list
         bis_after_1st_buy = [bi for bi in bis if bi.sdt.date() >= latest_1st_buy_point.date.date()]
         zs_seq_after_1st_buy = get_zs_seq(bis_after_1st_buy)
-        pprint.pprint(bis_after_1st_buy)
-        pprint.pp(zs_seq_after_1st_buy)
+
         if (
             0 < len(zs_seq_after_1st_buy) < 3
             and ubi['direction'] == Direction.Up
