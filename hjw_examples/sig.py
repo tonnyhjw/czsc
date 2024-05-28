f44dee2027650a4a61ba94b3121653f87c9a57d4
hjw <tonnyhjw@126.com>
2024-05-28 20:52:56 +0800

    bug fix: 最高笔需要向下

--- hjw_examples/sig.py
+++ hjw_examples/sig.py
@@ -77,7 +77,14 @@ def macd_pzbc_ubi(c: CZSC, fx_dt_limit: int = 30, **kwargs) -> OrderedDict:
     print(bi_a)
     print(bi_b)
     print(bi_a_macd_area, bi_b_macd_area)
+    print(zs2.is_valid)
+    print(ubi['direction'] == Direction.Up)
+    print(len(ubi['fxs']) < 2)
+    print(zs2.sdir == Direction.Down)
+    print(zs2.edir == Direction.Down)
+    print(zs2.dd == bi_b.low)
     print((0 > bi_b_dif > bi_a_dif or abs(bi_b_macd_area) < abs(bi_a_macd_area)))
+    print(estimated_profit >= 0.03)
 
     if (
             zs2.is_valid and
@@ -86,7 +93,7 @@ def macd_pzbc_ubi(c: CZSC, fx_dt_limit: int = 30, **kwargs) -> OrderedDict:
             zs2.sdir == Direction.Down and
             zs2.edir == Direction.Down and
             zs2.dd == bi_b.low and
-            (0 > bi_b_dif > bi_a_dif or abs(bi_b_macd_area) < abs(bi_a_macd_area)) and
+            (0 > bi_b_dif > bi_a_dif or abs(bi_a_macd_area)) > abs(bi_b_macd_area) and
             estimated_profit >= 0.03
     ):
         v1 = '一买'
