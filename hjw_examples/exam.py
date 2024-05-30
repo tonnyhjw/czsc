a4b0a7bace3b509b1afa28910adf0dc2227125c6
hjw <tonnyhjw@126.com>
2024-05-30 15:30:29 +0800

    add log debug 2nd buy

--- hjw_examples/exam.py
+++ hjw_examples/exam.py
@@ -4,7 +4,7 @@ from src.stock_process import *
 
 
 def play_day_trend_reverse():
-    row = dict(ts_code="300073.sh", symbol="300073", name="当升科技", industry="电气设备")
+    row = dict(ts_code="300073.sz", symbol="300073", name="当升科技", industry="电气设备")
     sdt, edt = "20180101", "20240327"
     result = trend_reverse_ubi_entry(row=row, sdt=sdt, edt=edt, freq="D", fx_dt_limit=5)
     pprint.pprint(result)
