4dd6c0c184c8a9512b11b5a7165850820333dcb3
hjw <tonnyhjw@126.com>
2024-06-18 13:46:31 +0800

    debug

--- hjw_examples/all_stocks_backtest.py
+++ hjw_examples/all_stocks_backtest.py
@@ -3,6 +3,7 @@
 import os
 import traceback
 import concurrent
+import backtrader as bt
 from pprint import pprint
 from datetime import datetime
 from concurrent.futures import ProcessPoolExecutor
@@ -45,24 +46,44 @@ def run_all_stocks_backtest(stock, edt: str = datetime.now().strftime('%Y%m%d'),
 
 
 def combine_trade_analyzers(analyzers):
-    combined = {}
+    combined = bt.AutoOrderedDict()
     for analyzer in analyzers:
-        for key, value in analyzer.items():
-            if key not in combined:
-                combined[key] = value
+        analyzer_data = analyzer.get_analysis()
+        for key, value in analyzer_data.items():
+            if key in combined:
+                if isinstance(value, dict):
+                    # 如果该键对应的值是字典,则递归合并
+                    combined[key] = combine_dicts(combined[key], value)
+                else:
+                    # 否则直接累加
+                    combined[key] += value
             else:
-                combined[key] += value
+                combined[key] = value
     return combined
 
 
 def combine_sharpe_ratios(ratios):
-    combined = {}
+    combined = bt.AutoOrderedDict()
     for ratio in ratios:
-        for key, value in ratio.items():
-            if key not in combined:
-                combined[key] = value
+        ratio_data = ratio.get_analysis()
+        for key, value in ratio_data.items():
+            if key in combined:
+                combined[key] = (combined[key] + value) / 2  # 计算平均值
             else:
-                combined[key] = (combined[key] + value) / 2  # 平均值
+                combined[key] = value
+    return combined
+
+
+def combine_dicts(dict1, dict2):
+    combined = dict1.copy()
+    for key, value in dict2.items():
+        if key in combined:
+            if isinstance(value, dict):
+                combined[key] = combine_dicts(combined[key], value)
+            else:
+                combined[key] += value
+        else:
+            combined[key] = value
     return combined
 
 
