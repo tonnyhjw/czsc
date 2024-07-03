8e0dd7ad86e8fd37bcbef74708f02d49365fc8a6
hjw <tonnyhjw@126.com>
2024-07-03 15:13:56 +0800

    debug

--- hjw_examples/exam.py
+++ hjw_examples/exam.py
@@ -5,8 +5,8 @@ from src.backtrade import run_single_stock_backtest
 
 
 def play_day_trend_reverse():
-    row = dict(ts_code="834033.bj", symbol="834033", name="康普化学", industry="化工原料")
-    sdt, edt = "20200101", "20240625"
+    row = dict(ts_code="688521.sh", symbol="688521", name="芯原股份", industry="半导体")
+    sdt, edt = "20200101", "20240702"
     result = trend_reverse_ubi_entry(row=row, sdt=sdt, edt=edt, freq="D", fx_dt_limit=5)
     pprint.pprint(result)
 
