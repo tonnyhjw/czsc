f9a85d6e9eb970285fd9051af88f0c6f6659abab
hjw <tonnyhjw@126.com>
2024-07-16 21:55:12 +0800

    引入命令参数

--- hjw_examples/day_ma_support.py
+++ hjw_examples/day_ma_support.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 import datetime
 import concurrent
 from loguru import logger
@@ -18,7 +19,7 @@ script_name = os.path.basename(__file__)
 logger.add("statics/logs/day_ma_support.log", rotation="10MB", encoding="utf-8", enqueue=True, retention="10 days")
 
 
-def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y%m%d')):
+def check(sdt: str = "20200101", edt: str = datetime.datetime.now().strftime('%Y%m%d'), subj_lv1="自动盯盘"):
     stock_basic = TsDataCache(home_path).stock_basic()  # 只用于读取股票基础信息
     total_stocks = len(stock_basic)
     results = []  # 用于存储所有股票的结果
@@ -38,17 +39,34 @@ def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y
             if result:
                 results.append(result)
 
-    email_subject = f"[测试][日线ma支撑][A股]{edt}发现{len(results)}个买点"
+    email_subject = f"[{subj_lv1}][日线ma支撑][A股]{edt}发现{len(results)}个买点"
     notify_buy_points(results=results, email_subject=email_subject, notify_empty=False)
 
 
 if __name__ == '__main__':
-    first_day = '2024-05-12'
-    last_day = '2024-06-12'
-    # last_day = datetime.datetime.now().strftime("%Y-%m-%d")
-    trade_dates = TsDataCache(home_path).get_dates_span(first_day, last_day, is_open=True)
+    sdt = '2024-03-22'
+    today = datetime.datetime.now().strftime("%Y%m%d")
 
-    # 将日期格式化为'%Y%m%d'
-    for business_date in trade_dates:
-        logger.info(f"测试日期:{business_date}")
-        check(sdt="20200101", edt=business_date)
+    parser = argparse.ArgumentParser(description="这是一个示例程序")
+    # 添加参数
+    parser.add_argument("sdt", nargs='?', default=sdt, help="开始日期")
+    parser.add_argument("edt", nargs='?', default=today, help="结束日期")
+    # 添加可选参数 -d 或 --dev
+    parser.add_argument("-d", "--dev", action="store_true", help="运行开发模式")
+
+    # 解析参数
+    args = parser.parse_args()
+
+    # 根据参数决定运行模式
+    if args.dev:
+        logger.info("正在运行开发模式")
+        logger.info(f"使用日期范围：{args.sdt} 到 {args.edt}")
+        trade_dates = TsDataCache(home_path).get_dates_span(args.sdt, args.edt, is_open=True)
+        # 将日期格式化为'%Y%m%d'
+        for business_date in trade_dates:
+            logger.info(f"测试日期:{business_date}")
+            check(edt=business_date)
+    else:
+        logger.info("正在运行默认模式")
+        check()
+
--- shell/dayly_dev.sh
+++ shell/dayly_dev.sh
@@ -4,7 +4,8 @@ source venv/bin/activate
 
 
 # 以最新数据分析当天走势
-python -m hjw_examples.day_trend_bc_reverse_dev
+#python -m hjw_examples.day_trend_bc_reverse_dev
+python -m hjw_examples.day_ma_support 20240521 20240716 -d
 
 # 执行回测
 python -m hjw_examples.all_stocks_backtest
