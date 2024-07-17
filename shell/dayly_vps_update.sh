61862ba9c62e3deb7fec228b861f212a503b1fed
hjw <tonnyhjw@126.com>
2024-07-17 18:05:01 +0800

    debug

--- shell/dayly_vps_update.sh
+++ shell/dayly_vps_update.sh
@@ -5,5 +5,6 @@ source venv/bin/activate
 # 移除缓存
 python -m hjw_examples.remove_cache
 # 以最新数据分析当天走势
-python -m hjw_examples.day_ma_support
 python -m hjw_examples.day_trend_bc_reverse
+python -m hjw_examples.day_ma_support
+
