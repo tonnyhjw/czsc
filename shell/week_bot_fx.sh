95bfec280cd829d0a88087a9008d143082b56202
hjw <tonnyhjw@126.com>
2024-07-20 20:39:34 +0800

    dev week ma support

--- shell/week_bot_fx.sh
+++ shell/week_bot_fx.sh
@@ -5,4 +5,4 @@ source venv/bin/activate
 # 检测本周强势底分型个股
 # python -m hjw_examples.week_bot_fx
 #python -m hjw_examples.week_trend_bc_reverse
-python -m hjw_examples.ma_support --f W --tp 60 --n 3
+python -m hjw_examples.ma_support --f W --tp 120 --n 3
---
+++ shell/weekly_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/workspace/czsc/
+source venv/bin/activate
+
+python -m hjw_examples.ma_support --f W --tp 120 --n 3 --sd 20240521 --ed 20240716 -d
+
+
