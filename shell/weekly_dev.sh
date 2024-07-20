4b17b0dc2c32f99701e9c077fec6987db08000a4
qcloud <ubuntu@localhost.localdomain>
2024-07-20 20:49:41 +0800

    update shell

--- shell/weekly_dev.sh
+++ shell/weekly_dev.sh
@@ -2,6 +2,6 @@
 cd ~/workspace/czsc/
 source venv/bin/activate
 
-python -m hjw_examples.ma_support --f W --tp 120 --n 3 --sd 20240521 --ed 20240716 -d
+python -m hjw_examples.ma_support --f W --tp 120 --n 3 --sd 20240208 --ed 20240716 -d
 
 
