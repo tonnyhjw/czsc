431fec25006a50c397ea2bc269d223fb83151376
hjw <tonnyhjw@126.com>
2024-07-22 13:08:15 +0800

    改进二三买判断条件

--- shell/dayly_dev.sh
+++ shell/dayly_dev.sh
@@ -4,8 +4,8 @@ source venv/bin/activate
 
 
 # 以最新数据分析当天走势
-#python -m hjw_examples.day_trend_bc_reverse_dev
-python -m hjw_examples.ma_support --sd 20240521 --ed 20240716 -d
+python -m hjw_examples.day_trend_bc_reverse_dev
+#python -m hjw_examples.ma_support --sd 20240521 --ed 20240716 -d
 
 # 执行回测
 python -m hjw_examples.all_stocks_backtest
