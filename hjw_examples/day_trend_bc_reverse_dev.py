0ed3377f2e5b20b0d640a80b352e8ce54ea3e836
hjw <tonnyhjw@126.com>
2024-07-22 15:52:20 +0800

    调试

--- hjw_examples/day_trend_bc_reverse_dev.py
+++ hjw_examples/day_trend_bc_reverse_dev.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     # # 生成日期范围，从2024年1月1日到今天
     # date_range = pd.date_range(start='2024-01-17', end=today, freq='B')
     # formatted_dates = date_range.strftime('%Y%m%d').tolist()
-    sdt, edt = '2024-03-25', '2024-04-12'
+    sdt, edt = '2024-04-10', '2024-05-01'
     today = datetime.datetime.now().strftime("%Y%m%d")
     trade_dates = TsDataCache(home_path).get_dates_span(sdt, edt, is_open=True)
 
