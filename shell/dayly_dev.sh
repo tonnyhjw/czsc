0ca39855bc5074a4dfaf4d7fc4b85d64824493d8
hjw <tonnyhjw@126.com>
2024-07-25 18:28:11 +0800

    update dev shell

--- shell/dayly_dev.sh
+++ shell/dayly_dev.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 # 以最新数据分析当天走势
 #python -m hjw_examples.day_trend_bc_reverse --sd 20240521 --ed 20240716 -d
 #python -m hjw_examples.ma_support --sd 20240521 --ed 20240716 -d
-#python -m hjw_examples.day_trend_bc_reverse_us --sd 20240722 --ed 20240723 -d
+python -m hjw_examples.day_trend_bc_reverse_us --sd 20240111 --ed 20240724 -d
 python -m hjw_examples.ma_support_us --sd 20240111 --ed 20240724 -d
 
 # 执行回测
