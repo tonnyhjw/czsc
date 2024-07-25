1c1783ea7eae4586dc3c382e6a9bf49e33a52320
hjw <tonnyhjw@126.com>
2024-07-25 09:52:52 +0800

    重构调试脚本

--- hjw_examples/day_trend_bc_reverse.py
+++ hjw_examples/day_trend_bc_reverse.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import datetime
+import argparse
 import concurrent
 from loguru import logger
 from concurrent.futures import ProcessPoolExecutor
@@ -12,6 +13,7 @@ from czsc import home_path
 from czsc.data import TsDataCache
 from src.notify import notify_buy_points
 from src.stock_process import trend_reverse_ubi_entry
+from src import is_friday
 
 idx = 1000
 script_name = os.path.basename(__file__)
@@ -26,11 +28,14 @@ logger.add("statics/logs/day_trend_bc_reverse.log", rotation="50MB", encoding="u
 # list_date     19910403
 # Name: 0, dtype: object
 
+tdc = TsDataCache(home_path)
 
-def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y%m%d')):
+
+def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y%m%d'), freq: str = 'D',
+          subj_lv1="自动盯盘"):
     os.environ['czsc_min_bi_len'] = '7'
 
-    stock_basic = TsDataCache(home_path).stock_basic()  # 只用于读取股票基础信息
+    stock_basic = tdc.stock_basic()  # 只用于读取股票基础信息
     total_stocks = len(stock_basic)
     results = []  # 用于存储所有股票的结果
 
@@ -41,7 +46,7 @@ def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y
             _today = datetime.datetime.today()
             logger.info(f"共{total_stocks}个股票，正在分析第{index}只个股{_ts_code}在{edt}的走势，"
                         f"进度{round(float(index/total_stocks)*100)}%")
-            future = executor.submit(trend_reverse_ubi_entry, row, sdt, edt, 'D', 5)
+            future = executor.submit(trend_reverse_ubi_entry, row, sdt, edt, freq, 5)
             futures[future] = _ts_code  # 保存future和ts_code的映射
 
         for future in concurrent.futures.as_completed(futures):
@@ -49,9 +54,41 @@ def check(sdt: str = "20180101", edt: str = datetime.datetime.now().strftime('%Y
             if result:
                 results.append(result)
 
-    email_subject = f"[自动盯盘][日线买点][A股]{edt}发现{len(results)}个买点"
+    email_subject = f"[{subj_lv1}][{tdc.freq_map.get(freq)}买点][A股]{edt}发现{len(results)}个买点"
+
     notify_buy_points(results=results, email_subject=email_subject, notify_empty=False)
 
 
 if __name__ == '__main__':
-    check()
+    ana_sdt = '20240322'
+    today = datetime.datetime.now().strftime("%Y%m%d")
+
+    parser = argparse.ArgumentParser(description="这是一个示例程序")
+    # 添加参数
+    parser.add_argument("--f", type=str, default="D", help="K线级别")
+    parser.add_argument("--sd", default=ana_sdt, help="分析开始日期")
+    parser.add_argument("--ed", default=today, help="分析结束日期")
+    parser.add_argument("-d", "--dev", action="store_true", help="运行开发模式")
+    parser.add_argument("-r", "--refresh", action="store_true", help="更新缓存")
+
+    # 解析参数
+    args = parser.parse_args()
+
+    # 判断是否更新缓存
+    if args.refresh:
+        tdc.clear()
+
+    # 根据参数决定运行模式
+    if args.dev:
+        logger.info("正在运行开发模式")
+        logger.info(f"使用日期范围：{args.sd} 到 {args.ed}")
+        trade_dates = tdc.get_dates_span(args.sd, args.ed, is_open=True)
+        # 将日期格式化为'%Y%m%d'
+        for business_date in trade_dates:
+            if args.f == "W" and not is_friday(business_date):
+                continue
+            logger.info(f"测试日期:{business_date}")
+            check(edt=business_date, freq=args.f, subj_lv1="测试")
+    else:
+        logger.info("正在运行默认模式")
+        check(freq=args.f)
--- shell/dayly_dev.sh
+++ shell/dayly_dev.sh
@@ -4,7 +4,7 @@ source venv/bin/activate
 
 
 # 以最新数据分析当天走势
-#python -m hjw_examples.day_trend_bc_reverse_dev
+#python -m hjw_examples.day_trend_bc_reverse --sd 20240521 --ed 20240716 -d
 #python -m hjw_examples.ma_support --sd 20240521 --ed 20240716 -d
 python -m hjw_examples.day_trend_bc_reverse_us --sd 20240722 --ed 20240723 -d
 
