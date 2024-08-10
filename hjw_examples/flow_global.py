eac1bea1a69a50d68eb371a2cfd2bb0b4b435a7d
hjw <tonnyhjw@126.com>
2024-08-10 14:10:48 +0800

    bug fix

---
+++ hjw_examples/flow_global.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import argparse
+import datetime
+import concurrent
+from loguru import logger
+from concurrent.futures import ProcessPoolExecutor
+
+
+sys.path.insert(0, '.')
+sys.path.insert(0, '..')
+from czsc import home_path, DataClient
+from czsc.data import TsDataCache
+from src.notify import notify_buy_points
+from src.sig.money_flow import money_flow_global
+
+script_name = os.path.basename(__file__)
+cache_path = os.getenv("TS_CACHE_PATH", os.path.expanduser("~/.ts_data_cache"))
+# dc = DataClient(url="http://api.tushare.pro", cache_path=cache_path)
+logger.add("statics/logs/flow.log", rotation="10MB", encoding="utf-8", enqueue=True, retention="10 days")
+
+
+def check(target_day: str = datetime.datetime.now().strftime('%Y%m%d'), n_days: int = 365,
+          subj_lv1="自动盯盘", notify_empty=True):
+    results = []
+    tdc = TsDataCache(home_path)
+    stock_basic = tdc.stock_basic()  # 只用于读取股票基础信息
+    with ProcessPoolExecutor(max_workers=2) as executor:
+        futures = {}
+        for index, row in stock_basic.iterrows():
+            _ts_code = row.get('ts_code')
+            _today = datetime.datetime.today()
+            future = executor.submit(money_flow_individual, _ts_code, target_day, n_days)
+            futures[future] = _ts_code  # 保存future和ts_code的映射
+
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                results.append(result)
+    email_subject = f"[{subj_lv1}][资金流向][A股]{target_day}发现{len(results)}个买点自参照资金流放大"
+    notify_buy_points(results=results, email_subject=email_subject, notify_empty=notify_empty)
+
+
+if __name__ == '__main__':
+    ana_sdt = '20240322'
+    _sdt = '20230101'
+    today = datetime.datetime.now().strftime("%Y%m%d")
+
+    parser = argparse.ArgumentParser(description="这是一个示例程序")
+    # 添加参数
+    parser.add_argument("--n", default=365, type=int, help="选取n天作为分析区间")
+    parser.add_argument("--tgd", default=today, type=str, help="目标日期")
+    parser.add_argument("--sd", default=ana_sdt, help="分析开始日期")
+    parser.add_argument("--ed", default=today, help="分析结束日期")
+    parser.add_argument("-d", "--dev", action="store_true", help="运行开发模式")
+    # parser.add_argument("-r", "--refresh", action="store_true", help="更新缓存")
+
+    # 解析参数
+    args = parser.parse_args()
+
+    # # 判断是否更新缓存
+    # if args.refresh:
+    #     TsDataCache(home_path).clear()
+
+    # 根据参数决定运行模式
+    if args.dev:
+        logger.info("正在运行开发模式")
+        logger.info(f"使用日期范围：{args.sd} 到 {args.ed}")
+        trade_dates = TsDataCache(home_path).get_dates_span(args.sd, args.ed, is_open=True)
+        # 将日期格式化为'%Y%m%d'
+        for business_date in trade_dates:
+            logger.info(f"测试日期:{business_date}")
+            check(business_date, args.n, subj_lv1="测试", notify_empty=False)
+    else:
+        logger.info(f"正在运行默认模式，目标日期：{args.tgd}，共{args.n}天")
+        check(args.tgd, args.n)
--- src/notify.py
+++ src/notify.py
@@ -73,5 +73,35 @@ def notify_buy_backtrader(trade_analysis, sharpe_ratio, email_subject: str):
         logger.error(f"发送结果出现报错，{e_msg}\nTraceback: {tb}")
 
 
+def notify_money_flow(results: list, email_subject: str, notify_empty: bool = True):
+    html_table = "<h1>没有发现买点</h1>"
+
+    def send_buy_point_style_email(raw_html):
+        styled_table = daily_email_style(raw_html)
+        # 发送电子邮件
+        send_email(styled_table, email_subject)
+
+    try:
+        if results:
+            # 将结果转换为 DataFrame
+            sorted_results = sorted(results, key=sort_by_industry, reverse=True)
+            sorted_results = sorted(sorted_results, key=sort_by_fx_pwr, reverse=True)
+            sorted_results = sorted(sorted_results, key=sort_by_signals)
+            df_results = pd.DataFrame(sorted_results)
+            # 生成 HTML 表格
+            html_table = df_results.to_html(classes='table table-striped table-hover', border=0, index=False,
+                                            escape=False)
+            # 发送电子邮件
+            send_buy_point_style_email(html_table)
+        elif notify_empty:
+            send_buy_point_style_email(html_table)
+        else:
+            logger.info(html_table)
+
+    except Exception as e_msg:
+        tb = traceback.format_exc()  # 获取 traceback 信息
+        logger.error(f"发送结果出现报错，{e_msg}\nTraceback: {tb}")
+
+
 if __name__ == '__main__':
     send_email('hello', 'hello')
