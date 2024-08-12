66507ecd4d662ef633b12a8245c7e3d9194359fa
hjw <tonnyhjw@126.com>
2024-08-12 14:46:10 +0800

    笔中枢上移将fake_bi条件改为要求最后一笔有两个顶两个底fx

--- src/sig/zs_elevate.py
+++ src/sig/zs_elevate.py
@@ -70,16 +70,17 @@ def third_buy_bi(c: CZSC, fx_dt_limit: int = 5, **kwargs) -> OrderedDict:
         return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
 
     estimated_profit = (zs2.zg - cur_price) / cur_price
-
-    zs2_fake_bis = zs2.bis[-1].fake_bis
-    print(f"{len(zs2_fake_bis)=}")
+    zs2_bi = zs2.bis[0]
+    zs2_bi_fx_bot = [_fx for _fx in zs2_bi.fxs if _fx.mark == Mark.D]
+    zs2_bi_fx_top = [_fx for _fx in zs2_bi.fxs if _fx.mark == Mark.G]
 
     third_buy_conditions = (
         (ubi['direction'] == Direction.Up, "ubi['direction'] == Direction.Up"),
         (len(ubi['fxs']) < 2, "len(ubi['fxs']) < 2"),
-        (zs2.bis[0] == Direction.Down, "zs2.bis[0] == Direction.Down"),
+        (zs2_bi == Direction.Down, "zs2_bi == Direction.Down"),
         (len(zs2.bis) == 1, "len(zs2.bis) == 1"),
-        (3 <= len(zs2_fake_bis), "3 <= len(zs2_fake_bis)"),
+        (2 <= len(zs2_bi_fx_bot), "3 <= len(zs2_bi_fx_bot)"),
+        (2 <= len(zs2_bi_fx_top), "3 <= len(zs2_bi_fx_top)"),
         (zs1.zg < latest_fx.low, f"{zs1.zg=} < {latest_fx.low=}"),
     )
     failed_third_buy_conditions = select_failed_conditions(third_buy_conditions)
