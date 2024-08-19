c5050d7c80f6ec3236bc2669c40cbb0fbc8f988a
qcloud <ubuntu@localhost.localdomain>
2024-08-19 22:09:42 +0800

    update shell

--- shell/dayly_dev.sh
+++ shell/dayly_dev.sh
@@ -4,7 +4,8 @@ source venv/bin/activate
 
 
 # 以最新数据分析当天走势
-python -m hjw_examples.day_trend_bc_reverse --sd 20220101 --ed 20221231 -d
+# python -m hjw_examples.day_trend_bc_reverse --sd 20220101 --ed 20221231 -d
+python -m hjw_examples.elevate --t bi --sd 20240330 --ed 20240816 -d
 python -m hjw_examples.flow_global --n 7 --h 100 --sd 20220101 --ed 20221231 -d
 #python -m hjw_examples.ma_support --sd 20240521 --ed 20240716 -d
 #python -m hjw_examples.day_trend_bc_reverse_us --sd 20240111 --ed 20240724 -d
