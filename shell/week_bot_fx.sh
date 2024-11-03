4436cff5c49c1b4a1e0cb2394f55a030f0e33edf
qcloud <ubuntu@localhost.localdomain>
2024-11-03 10:30:51 +0800

    控制分型limit为1

--- shell/week_bot_fx.sh
+++ shell/week_bot_fx.sh
@@ -4,5 +4,5 @@ source venv/bin/activate
 
 # 检测本周强势底分型个股
 # python -m hjw_examples.week_bot_fx
-python -m hjw_examples.pzbc --f W --l 7
+python -m hjw_examples.pzbc --f W --l 1
 python -m hjw_examples.ma_support --f W --tp 120 --n 3
