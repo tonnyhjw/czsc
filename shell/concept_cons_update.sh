f6c00f3986518a6dfaf859f4aff5b2ee6a33c239
hjw <tonnyhjw@126.com>
2024-12-01 18:09:34 +0800

    添加概念板块得shell脚本

---
+++ shell/concept_cons_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/workspace/czsc/
+source venv/bin/activate
+
+# 板块概念排名变化
+python -m hjw_examples.concept_update -c
---
+++ shell/concept_radar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/workspace/czsc/
+source venv/bin/activate
+
+# 执行概念板块监控
+python -m hjw_examples.concept_radar
