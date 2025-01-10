00abeeeae37e173f76dad396ee1f47a9a8aa9699
qcloud <ubuntu@localhost.localdomain>
2025-01-10 15:01:35 +0800

    reconfig json path

--- src/concept/configs.py
+++ src/concept/configs.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 EXCLUDE_CODES = ["BK0815", "BK0816", "BK0817", "BK1050", "BK1051"]
 
-LIKED_CONCEPTS = Path(__file__).parent / 'statics' / 'liked_concepts.json'
+LIKED_CONCEPTS = Path(__file__).parent.parent.parent / 'statics' / 'liked_concepts.json'
