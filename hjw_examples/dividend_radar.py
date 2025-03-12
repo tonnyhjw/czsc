744c4e442c9693710a4e03baab09641923a0eb69
hjw <tonnyhjw@126.com>
2025-03-12 17:10:00 +0800

    将邮件表头改为中文

--- hjw_examples/dividend_radar.py
+++ hjw_examples/dividend_radar.py
@@ -9,6 +9,49 @@ from src.notify import notify_concept_radar
 SUBJ_LV1 = "自动盯盘"   # 或改为"测试"
 EDT: str = datetime.datetime.now().strftime('%Y%m%d')
 
+mapping = {
+    # 接口：dividend的字段
+    'ts_code': 'TS代码',
+    'end_date': '分红年度',
+    'ann_date': '预案公告日',
+    'div_proc': '实施进度',
+    'stk_div': '每股送转',
+    'stk_bo_rate': '每股送股比例',
+    'stk_co_rate': '每股转增比例',
+    'cash_div': '每股分红（税后）',
+    'cash_div_tax': '每股分红（税前）',
+    'record_date': '股权登记日',
+    'ex_date': '除权除息日',
+    'pay_date': '派息日',
+    'div_listdate': '红股上市日',
+    'imp_ann_date': '实施公告日',
+    'base_date': '基准日',
+    'base_share': '基准股本（万）',
+
+    # 接口：daily_basic的字段
+    'trade_date': '交易日期',
+    'close': '当日收盘价',
+    'turnover_rate': '换手率（%）',
+    'turnover_rate_f': '换手率（自由流通股）',
+    'volume_ratio': '量比',
+    'pe': '市盈率（总市值/净利润，亏损的PE为空）',
+    'pe_ttm': '市盈率（TTM，亏损的PE为空）',
+    'pb': '市净率（总市值/净资产）',
+    'ps': '市销率',
+    'ps_ttm': '市销率（TTM）',
+    'dv_ratio': '股息率（%）',
+    'dv_ttm': '股息率（TTM）（%）',
+    'total_share': '总股本（万股）',
+    'float_share': '流通股本（万股）',
+    'free_share': '自由流通股本（万）',
+    'total_mv': '总市值（万元）',
+    'circ_mv': '流通市值（万元）',
+
+    # 常用字段
+    'stock_name': '股票名',
+    'top_concepts': '概念题材'
+}
+
 def run(concept_top_n: int = 30, concept_rank_threshold: int = 10):
     selector = dividend.DividendStockSelector()
     trade_date = datetime.datetime.now().strftime('%Y%m%d')
@@ -45,6 +88,8 @@ def run(concept_top_n: int = 30, concept_rank_threshold: int = 10):
     else:
         email_subject = f"[{SUBJ_LV1}][分红][A股]{EDT}发现{len(results_df)}个个股今天分红除权"
         results_df = utils.embed_ts_code_href(results_df)
+        results_df = results_df.rename(columns=mapping)
+
         notify_concept_radar(results_df, email_subject)
 
 
