80a263b8873b9059c7efc3dbb4f17d0c1c64f493
hjw <tonnyhjw@126.com>
2025-03-19 11:47:33 +0800

    dividend radar添加调试模式

--- hjw_examples/dividend_radar.py
+++ hjw_examples/dividend_radar.py
@@ -1,13 +1,17 @@
 import datetime
+import argparse
+from loguru import logger
 
+from czsc import home_path
+from czsc.data import TsDataCache
 from src.sig import dividend
 from src.concept import utils
 from src.concept.hot_rank import ConceptHotRank, RankType
 from src.notify import notify_concept_radar
 
+logger.add("statics/logs/dividend.log", rotation="5MB", encoding="utf-8", enqueue=True, retention="10 days")
 
 SUBJ_LV1 = "自动盯盘"   # 或改为"测试"
-EDT: str = datetime.datetime.now().strftime('%Y%m%d')
 
 mapping = {
     # 接口：dividend的字段
@@ -53,10 +57,11 @@ mapping = {
     'top_concepts': '概念题材'
 }
 
-def run(concept_top_n: int = 30, concept_rank_threshold: int = 10):
+def run(trade_date: str=datetime.datetime.now().strftime('%Y%m%d'),
+        concept_top_n: int = 30,
+        concept_rank_threshold: int = 10,
+        subj_lv1: str = SUBJ_LV1):
     selector = dividend.DividendStockSelector()
-    trade_date = datetime.datetime.now().strftime('%Y%m%d')
-
 
     # 选择市值小于50亿元的股票
     selected_stocks = selector.select_stocks(
@@ -87,7 +92,7 @@ def run(concept_top_n: int = 30, concept_rank_threshold: int = 10):
     if results_df.empty:
        print("没有符合条件的分红个股")
     else:
-        email_subject = f"[{SUBJ_LV1}][分红][A股]{EDT}发现{len(results_df)}个个股今天分红除权"
+        email_subject = f"[{subj_lv1}][分红][A股]{trade_date}发现{len(results_df)}个个股今天分红除权"
         results_df = utils.embed_ts_code_href(results_df)
         results_df = results_df.rename(columns=mapping)
 
@@ -95,4 +100,27 @@ def run(concept_top_n: int = 30, concept_rank_threshold: int = 10):
 
 
 if __name__ == '__main__':
+    today = datetime.datetime.now().strftime('%Y%m%d')
+
+    parser = argparse.ArgumentParser(description="分红监控")
+    parser.add_argument("-d", "--dev", action="store_true", help="运行开发模式")
+    parser.add_argument("--sd", default="20250301", help="开发模式调试起始日")
+    parser.add_argument("--ed", default=today, help="开发模式调试结束日")
+
     run()
+
+    # 解析参数
+    args = parser.parse_args()
+
+    # 根据参数决定运行模式
+    if args.dev:
+        logger.info("正在运行开发模式")
+        logger.info(f"使用日期范围：{args.sd} 到 {args.ed}")
+        trade_dates = TsDataCache(home_path).get_dates_span(args.sd, args.ed, is_open=True)
+        # 将日期格式化为'%Y%m%d'
+        for business_date in trade_dates:
+            logger.info(f"调试日期:{business_date}")
+            run(trade_date=business_date, subj_lv1="测试")
+    else:
+        logger.info("正在运行默认模式")
+        run(trade_date=today)
