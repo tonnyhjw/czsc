299572455fd83c9b7eef1f2caa142b6503bc36a8
hjw <tonnyhjw@126.com>
2025-05-19 14:03:04 +0800

    添加模拟二买运行程序

--- src/stock_process.py
+++ src/stock_process.py
@@ -6,7 +6,7 @@ import traceback
 from czsc import CZSC, home_path
 from czsc.data import TsDataCache
 from src.connectors.yf_cache import YfDataCache
-from src.sig.trend_reverse import trend_reverse_bi, trend_reverse_xd
+from src.sig.trend_reverse import trend_reverse_bi, fake_xd_2nd_buy_point
 from src.sig.pzbc import macd_pzbc_bi
 from src.sig.powers import long_term_ma_support, long_term_ma_up
 from src.sig.zs_elevate import third_buy_bi, third_buy_xd
@@ -321,3 +321,43 @@ def row_2_czsc(row, sdt, edt, freq: str = 'W'):
     dc = TsDataCache(home_path)  # 在每个进程中创建独立的实例
     bars = dc.pro_bar(_ts_code, start_date=sdt, end_date=edt, freq=freq, asset="E", adj='qfq', raw_bar=True)
     return CZSC(bars)
+
+
+def fake_xd_2nd_buy(row, sdt, edt, freq: str, fx_dt_limit: int = 5):
+    dc = TsDataCache(home_path)  # 在每个进程中创建独立的实例
+    _ts_code = row.get('ts_code')
+    _symbol = row.get('symbol')
+    _name = row.get('name')
+    _industry = row.get("industry")
+    _hs = _ts_code.split(".")[-1]
+    _edt = datetime.datetime.strptime(edt, "%Y%m%d")
+
+    output = {}
+    try:
+        bars = dc.pro_bar(_ts_code, start_date=sdt, end_date=edt, freq=freq, asset="E", adj='qfq', raw_bar=True)
+        # if "ST" in _name:
+        #     return output
+        c = CZSC(bars)
+        _signals = fake_xd_2nd_buy_point(c, edt=_edt, fx_dt_limit=fx_dt_limit, freq=freq, **row)
+        logger.debug(_signals)
+
+        for s_value in _signals.values():
+            if "买" in s_value:
+                s_value_detail = s_value.split("_")
+                symbol_link = f'<a href="https://xueqiu.com/S/{_hs}{_symbol}">{_symbol}</a>'
+                output = {
+                    'name': _name,
+                    'symbol': symbol_link,
+                    'ts_code': _ts_code,
+                    'signals': s_value_detail[0],
+                    'fx_pwr': s_value_detail[1],
+                    'expect_profit(%)': round(float(s_value_detail[2]) * 100, 2),
+                    'industry': _industry
+                }
+    except Exception as e_msg:
+        tb = traceback.format_exc()  # 获取 traceback 信息
+        logger.critical(f"{_ts_code} {_name}出现报错，{e_msg}\nTraceback: {tb}")
+
+    finally:
+        gc.collect()
+        return output
