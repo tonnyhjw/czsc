2cea19733b69407fee17e6911d38a7da133b2ab3
hjw <tonnyhjw@126.com>
2025-05-19 13:27:52 +0800

    初始化模拟线段二买

--- src/sig/trend_reverse.py
+++ src/sig/trend_reverse.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 from czsc import CZSC
 from czsc.objects import Direction, FX, BI, ZS
 from czsc.utils import get_sub_elements, create_single_signal
-from czsc.signals.tas import update_macd_cache
+from czsc.signals.tas import update_macd_cache, update_ma_cache
 from czsc.enum import Mark
 from database import history
 from src.sig.utils import *
@@ -296,4 +296,108 @@ def trend_reverse_xd(c: CZSC, fx_dt_limit: int = 5, **kwargs) -> OrderedDict:
 
     gc.collect()
 
+    return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
+
+def fake_xd_2nd_buy_point(c: CZSC, fx_dt_limit: int = 5, **kwargs) -> OrderedDict:
+    """模拟线段二买
+    **信号逻辑：**
+
+    1. 三个中枢按方向“下上下”排列
+    2. 第一个中枢（向下）的DD是三个中枢的最低点
+    3. 第二个中枢（向上）
+        1. 第一笔至少要回到第一中枢内部
+        2. 第一笔的low就是该中枢的DD
+    4. 第三个中枢（向下）
+        1. 最后底分型收到MA250支撑，底分型第三元素最终收盘价高于MA250
+        2. 排除弱底分型
+        3. 至少3笔
+
+    主要用于用于配合热门题材监控捕捉机会
+
+    :param c: CZSC对象
+    :param fx_dt_limit: int, 分型时效性限制
+    :param kwargs:
+    :return: 信号识别结果
+    """
+    db = kwargs.get("db", "BI")
+    freq = c.freq.value
+    v1 = '其他'
+    edt = kwargs.get('edt', datetime.datetime.now())
+    name, ts_code, symbol = kwargs.get('name'), kwargs.get('ts_code'), kwargs.get('symbol')
+    k1, k2, k3 = freq, symbol, edt.strftime("%Y%m%d")
+    industry, freq = kwargs.get('industry'), kwargs.get('freq')
+
+
+    ubi = c.ubi
+    bis = c.bi_list
+    cur_price = c.bars_raw[-1].close
+    latest_fx = c.ubi_fxs[-1]  # 最近一个分型
+    fx_is_exceed = date_exceed_rawbars(c.bars_raw, latest_fx.dt, fx_dt_limit)
+
+    if len(bis) < 5 or not ubi or len(ubi['raw_bars']) < 3:
+        v1 = 'K线不合标准'
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+    if latest_fx.mark != Mark.D or fx_is_exceed:
+        v1 = '没有底分型'
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+    elif history.buy_point_exists(symbol, latest_fx.dt, freq, db):
+        v1 = '已存在'
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+    else:
+        v2 = latest_fx.power_str
+
+    zs_seq = get_zs_seq_change_limited(bis)
+    if len(zs_seq) < 3:
+        v1 = '中枢不够3个'
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
+    zs1, zs2, zs3 = zs_seq[-3], zs_seq[-2], zs_seq[-1]
+    if len(zs1.bis) < 3 or len(zs2.bis) < 3 or len(zs3.bis) < 3:
+        v1 = '中枢笔不足'
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
+    estimated_profit = (zs2.zg - cur_price) / cur_price
+
+    def _zs_direction(zs_direction: Direction, zs: ZS):
+        return zs.sdir == zs_direction and zs.edir == zs_direction
+
+    def _zs1_ending_has_min_dd():
+        return zs1.dd == min(zs1.dd, zs2.dd, zs3.dd) == zs1.bis[-1].low
+
+
+    zs1_direction_down = _zs_direction(Direction.Down, zs1)
+    zs2_direction_up = _zs_direction(Direction.Up, zs2)
+    zs3_direction_down = _zs_direction(Direction.Down, zs3)
+    zs1_ending_has_min_dd = _zs1_ending_has_min_dd()
+
+
+    fake_xd_2nd_buy_conditions = (
+        (ubi['direction'] == Direction.Up, "ubi['direction'] == Direction.Up"),
+        (len(ubi['fxs']) < 2, f"{len(ubi['fxs'])=} < 2"),
+        (zs1_direction_down, f"{zs1_direction_down=}"),
+        (zs2_direction_up, f"{zs2_direction_up=}"),
+        (zs3_direction_down, f"{zs3_direction_down=}"),
+        (zs1_ending_has_min_dd, f"{zs1_ending_has_min_dd=}"),
+        (zs2.bis[0].high > zs1.zd, f"{zs2.bis[0].high=} > {zs1.zd=} fail"),
+
+    )
+    failed_fake_xd_2nd_buy_conditions = select_failed_conditions(fake_xd_2nd_buy_conditions)
+
+    if not failed_fake_xd_2nd_buy_conditions:
+        v1 = '二买FXD'
+        bars_raw = c.bars_raw
+        ma120 = update_ma_cache(c, ma_type="SMA", timeperiod=120)
+        ma250 = update_ma_cache(c, ma_type="SMA", timeperiod=250)
+        zs3_last_raw_bar = bars_raw[-1]
+        if (zs3_last_raw_bar.close > zs3_last_raw_bar.cache[ma250]
+                and zs3_last_raw_bar.cache[ma120] > zs3_last_raw_bar.cache[ma250]) \
+                and v2 != '弱':
+
+            # 插入数据库
+            history.insert_buy_point(name, symbol, ts_code, freq, v1, latest_fx.power_str, estimated_profit, industry,
+                                     latest_fx.dt, latest_fx.high, latest_fx.low, reason="fake_xd_2nd_buy", db=db)
+            return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2, v3=estimated_profit)
+    else:
+        logger.info(f"{name}{symbol}二买Fake不成立原因: {failed_fake_xd_2nd_buy_conditions}")
     return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
