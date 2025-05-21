80b68449c5aae25d262fc323ab3f84c87fab068a
hjw <tonnyhjw@126.com>
2025-05-21 10:26:23 +0800

    关注题材增加时间戳命令--dt

--- hjw_examples/concept_liked.py
+++ hjw_examples/concept_liked.py
@@ -1,5 +1,15 @@
+import argparse
+import datetime
 
+from hjw_examples.concept_radar import liked
 
 if __name__ == '__main__':
-    from hjw_examples.concept_radar import liked
-    liked(bp_days_limit=3)
+    today = datetime.datetime.now().strftime('%Y%m%d')
+
+    parser = argparse.ArgumentParser(description="关注题材监控")
+    parser.add_argument("--dt", default=today, help="目标日期")
+
+    # 解析参数
+    args = parser.parse_args()
+
+    liked(bp_days_limit=3, latest_timestamp=args.dt)
