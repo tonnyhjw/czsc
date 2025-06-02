b8e4778b182efb0724f731a4c246f28396cf4285
qcloud <ubuntu@localhost.localdomain>
2025-06-02 13:51:07 +0800

    将模拟二买检测调整到最前，并回复每日刷新数据，修复连续多日显示相同结果的问题

--- shell/dayly_vps_update.sh
+++ shell/dayly_vps_update.sh
@@ -7,13 +7,13 @@ source venv/bin/activate
 # 以最新数据分析当天走势
 # python -m hjw_examples.ma_support -r
 
+python -m hjw_examples.fake_xd_2nd_buy -r
+
 python -m hjw_examples.day_trend_bc_reverse --f D
 
 python -m hjw_examples.elevate --t bi
 
 python -m hjw_examples.flow_global --n 7 --h 100
-
-python -m hjw_examples.fake_xd_2nd_buy
 
 python -m hjw_examples.dividend_radar
 
