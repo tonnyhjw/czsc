c7efc0b85c04d1f31ad904faa0dbac216f2cdb35
hjw <tonnyhjw@126.com>
2025-06-11 12:26:37 +0800

    将ma_aligned_bullish应用到zs elevate

--- src/sig/zs_elevate.py
+++ src/sig/zs_elevate.py
@@ -10,7 +10,7 @@ from czsc.utils import create_single_signal
 from czsc.utils.sig import get_zs_seq
 from czsc.enum import Mark
 from database import history
-from src.sig.utils import select_failed_conditions, date_exceed_rawbars, get_xd_zs_seq, get_zs_seq_change_limited
+from src.sig.utils import *
 from src.xd.analyze_by_break import analyze_xd
 
 
@@ -100,10 +100,10 @@ def third_buy_bi(c: CZSC, fx_dt_limit: int = 5, **kwargs) -> OrderedDict:
 
     if not failed_third_buy_conditions:
         v1 = '三买ZE'
-        # 插入数据库
-        history.insert_buy_point(name, symbol, ts_code, freq, v1, latest_fx.power_str, estimated_profit,
-                                 industry, latest_fx.dt, latest_fx.high, latest_fx.low, reason="zs_elevate", db=db)
-        if v2 != '弱':
+        if ma_aligned_bullish(c, close_above_long_term_ma=True) and v2 != '弱':
+            # 插入数据库
+            history.insert_buy_point(name, symbol, ts_code, freq, v1, latest_fx.power_str, estimated_profit,
+                                     industry, latest_fx.dt, latest_fx.high, latest_fx.low, reason="zs_elevate", db=db)
             return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2, v3=estimated_profit)
     else:
         logger.info(f"{name}{symbol}中枢上移三买不成立原因: {failed_third_buy_conditions}")
